fc06804e42197f8a6e85b24923f1daccbfe13492
Max Blancarte <blancarte@gmail.com>
2022-06-16 22:59:19 -0700

    users crud

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/kublick/goadmin/database"
 	"github.com/kublick/goadmin/models"
@@ -30,4 +32,44 @@ func CreateUser(c *fiber.Ctx) error {
 
 	return c.JSON(user)
 
+}
+
+func GetUser(c *fiber.Ctx) error {
+
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{Id: uint(id)}
+
+	database.DB.Find(&user)
+
+	return c.JSON(user)
+
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{Id: uint(id)}
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	database.DB.Model(&user).Updates(user)
+
+	return c.JSON(user)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{Id: uint(id)}
+
+	database.DB.Delete(&user)
+
+	return c.JSON(fiber.Map{
+		"message": "User deleted",
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -16,4 +16,8 @@ func Setup(app *fiber.App) {
 	app.Post("/api/logout", controllers.Logout)
 	app.Get("/api/users", controllers.AllUsers)
 	app.Post("/api/users", controllers.CreateUser)
+
+	app.Get("/api/users/:id", controllers.GetUser)
+	app.Put("/api/users/:id", controllers.UpdateUser)
+	app.Delete("/api/users/:id", controllers.DeleteUser)
 }
